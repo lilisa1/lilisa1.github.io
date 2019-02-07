0686d8a3d3289dbf301748b6ddf6f93ca55751e6
Lisa Li <lisali@Lisa-Li.local>
2019-02-07 15:10:17 -0500

    HW 4 Update

--- personal/elana/iloveu.js
+++ personal/elana/iloveu.js
@@ -12,12 +12,14 @@ window.onload = function() {
         secondsInAHour = 60 * 60 * 1000;
       
     days = Math.floor(timeDifference / (secondsInADay) * 1);
+    months = Math.floor(timeDifference / (secondsInADay) * 1 / 30);
     hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
     mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
     secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
   
     var idEl = document.getElementById(id);
     idEl.getElementsByClassName('days')[0].innerHTML = days;
+    idEl.getElementsByClassName('months')[0].innerHTML = months;
     idEl.getElementsByClassName('hours')[0].innerHTML = hours;
     idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
     idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
