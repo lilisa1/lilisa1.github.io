841773ab263ff9eef971546c4e8ffef8f8613199
Lisa Li <lisali@Lisa-Li.local>
2019-02-21 20:20:39 -0500

    Classwork

--- in class exercises/datafile.js
+++ in class exercises/datafile.js
@@ -1,22 +1,47 @@
-console.log("Welcome to Data Land");
-
-//for txt
-d3.text("load data/text.txt", function(error, data) {
-    console.log("error:", error);
-    console.log("text: ", data);
-});
-
 //for csv
+
+var countryNames = {
+    "us": "United States",
+    "mx": "Mexico",
+    "cn": "Canada"
+};
+
 d3.csv("load data/csv.csv", function(error, data) {
-    console.log("csv: ", data);
-    data.forEach(function(d) {
+
+    data.forEach(function(d){
         d.export = parseFloat(d.export);
+        d.source = "Lisa Made This";
+        d.fullName = countryNames[d,country];
+        console.log(d);
     });
+
+    var currentData = data.filter(function(d){
+        return d.year == "2018";
+    });
+    console.log(currentData);
+
+    var dataExtent = d3.extent(data, function(d){
+        return d.export;
+    });
+    console.log(dataExtent);
+
+    var groupedCountries = d3.nest()
+        .key(function(d){
+            return d.country;
+        })
+        .entries(data);
+        
+    groupedCountries.forEach(function(d){
+        d.total = d3.sum(d.values, function(row){
+            return row.export;
+        });
+    });
+
+    var yearlyData = d3.nest()
+        .key(function(row){
+            return row.year;
+        })
+        .entries(data);
+        console.log("yearlyData", yearlyData)
 });
 
-
-//for json
-d3.json("load data/json.json", function(error, data) {
-    console.log("error:", error);
-    console.log("json: ", data);
-});
