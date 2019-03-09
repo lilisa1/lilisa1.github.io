1562cb311c68bdff9c82fe352c7f5384d6766a72
Lisa Li <lisali@Lisa-Li.local>
2019-03-08 21:05:52 -0500

    Project Update

--- final project/finalproject.js
+++ final project/finalproject.js
@@ -1,4 +1,51 @@
-d3.json("finalproject.json", function(error, data) {
-    console.log("error:", error);
-    console.log("json:", data);
-});
+
+var width = 1000;
+var height = 700;
+
+var svg = d3.select( "#mapcontainer" )
+    .append( "svg" )
+    .attr( "width", width )
+    .attr( "height", height );
+
+var g = svg.append( "g" );
+
+
+
+var albersProjection = d3.geoAlbers()
+    .scale( 190000 )
+    .rotate( [71.057,0] )
+    .center( [0, 42.313] )
+    .translate( [width/2,height/2] );
+
+var geoPath = d3.geoPath()
+    .projection( albersProjection );
+
+
+
+g.selectAll( "path" )
+    .data( neighborhoods_json.features )
+    .enter()
+    .append( "path" )
+    .attr( "fill", "#ccc" )
+    .attr( "stroke", "#333")
+    .attr( "d", geoPath );
+
+var players = svg.append( "g" );
+
+players.selectAll( "path" )
+    .data( playerinfo_json.features )
+    .enter()
+    .append( "path" )
+    .attr( "fill", "#900" )
+    .attr( "stroke", "#999" )
+    .attr( "r", "50")
+    .attr( "d", geoPath )
+    .attr( "class", "mapdot")
+    .on("mouseover", function(d){
+      d3.select("h3").text(d.properties.Name);
+      d3.select(this).attr("class","mapdot hover");
+    })
+    .on("mouseout", function(d){
+      d3.select("h3").text("");
+      d3.select(this).attr("class","mapdot");
+    });
