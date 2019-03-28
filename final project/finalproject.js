d3457e7a7790474c51cf24d64f30acc5b2e7b757
Lisa Li <lisali@Lisa-Li.local>
2019-03-27 23:58:04 -0400

    Tooltip Edit

--- final project/finalproject.js
+++ final project/finalproject.js
@@ -56,14 +56,15 @@ d3.queue()
 //loading json for points indicating which colleges players went to
     d3.json("finalproject.json", function(data) { 
 
+    var groupCollege = d3.nest()
+    .key(function(d) { return d.Coordinates; })
+    .rollup(function(v) { return v.length; })
+    .entries(data);
+
       var cities = svg.selectAll("circle")
         .data(data);
       
       //grouping players by coordinate of college and calculating value of grouped array
-      var groupCollege = d3.nest()
-        .key(function(d) { return d.Coordinates; })
-        .rollup(function(v) { return v.length; })
-        .entries(data);
 
         console.log(groupCollege); //<----
 
@@ -76,17 +77,25 @@ d3.queue()
             .attr("fill", "rgba(255, 106, 106, 0.8)")
             .on("mousemove", function(d) {
                 var mouse = d3.mouse(document.body);
+                d3.select(this)
+                    .attr("r", 9)
+                    .style("fill", "rgba(252, 147, 61, 0.849)");
                 d3.select("#tooltip")
                     .style("display", "block")
                     .html("<h1>" + d.Name + "</h1>" + 
                           "<h3>" + d.College + "</h3>" +
-                          "<h4>" + d.Club + "</h4>")
-                    .style("left", mouse[0] + 10 + "px")
+                          "<h4>" + d.Club + "</h4>" +
+                          "<img src=" + d.Photo + ">")
+                    .style("left", mouse[0] + 25 + "px")
                     .style("top", mouse[1] - 55 + "px");
+                    
       })
       .on("mouseout", function(d) {
-          d3.select("#tooltip")
-              .style("display", "none")
+        d3.select(this)
+            .attr("r", 6)
+            .style("fill", "rgba(255, 106, 106, 0.8)");
+        d3.select("#tooltip")
+            .style("display", "none")
       });
 
       })
